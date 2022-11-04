1cb46765449a43ca213b3ee0b3c52fd5a9b89d73
Soumyaranjan Parhi <parhisoumyaranjan02@gmail.com>
2022-11-04 10:52:17 +0530

    cloudniary commit2

--- Server/User/cloudinary/index.js
+++ Server/User/cloudinary/index.js
@@ -1,20 +1,4 @@
-// require('dotenv').config();
-// const cloudinary=require('cloudinary').v2;
-// const { CloudinaryStorage }=require('multer-storage-cloudinary');
-// cloudinary.config({
-//     cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-//     api_key:process.env.CLOUDINARY_KEY,
-//     api_secret:process.env.CLOUDINARY_SECRET
-// })
-// const storage=new CloudinaryStorage({
-//     cloudinary,
-//     params:{
-//         folder:'RX',
-//         allowedFormats:['jpeg','png','jpg']
-//     }
-    
-// });
-//  module.exports={cloudinary, storage};
+
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 cloudinary.config({ 
